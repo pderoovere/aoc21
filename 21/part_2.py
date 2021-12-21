70256e9e7ccf274728a29f3bc7641860436c9c98
Peter De Roovere <peter.deroovere@robojob.eu>
2021-12-21 20:20:57 +0100

    Cosmetic changes

--- 21/part_2.py
+++ 21/part_2.py
@@ -2,13 +2,13 @@ from pathlib import Path
 from collections import defaultdict
 from dataclasses import dataclass
 from functools import lru_cache, reduce
+import operator
 
-plays = defaultdict(int)
+moves = defaultdict(int)
 for i in range(1, 4):
     for j in range(1, 4):
         for k in range(1, 4):
-            nb = i + j + k
-            plays[nb] = plays[nb] + 1
+            moves[i + j + k] += 1
 
 @dataclass(unsafe_hash=True)
 class State:
@@ -18,16 +18,16 @@ class State:
     player_2_score: int
     active_player: int
 
-    def increase(self, pos, increase):
+    def __increase_pos(self, pos, increase):
         return ((pos + increase - 1) % 10 + 1)
 
     def play(self, increase):
         if self.active_player == 1:
-            player_1_pos = self.increase(self.player_1_pos, increase)
+            player_1_pos = self.__increase_pos(self.player_1_pos, increase)
             player_1_score = self.player_1_score + player_1_pos
             return State(player_1_pos, player_1_score, self.player_2_pos, self.player_2_score, 2)
         else:
-            player_2_pos = self.increase(self.player_2_pos, increase)
+            player_2_pos = self.__increase_pos(self.player_2_pos, increase)
             player_2_score = self.player_2_score + player_2_pos
             return State(self.player_1_pos, self.player_1_score, player_2_pos, player_2_score, 1)
 
@@ -49,10 +49,10 @@ def simulate(state):
     elif state.player_2_score >= 21:
         return Outcomes(0, 1)
     else:
-        outcomes = [simulate(state.play(increase)) * count for increase, count in plays.items()]
-        return reduce(lambda o1, o2: o1 + o2, outcomes)
+        outcomes = [simulate(state.play(increase)) * count for increase, count in moves.items()]
+        return reduce(operator.add, outcomes)
 
-# First fill up the cache, starting with almost-finished states
+# Fill up the cache, starting with almost-finished states
 for p1_pos in range(10, 0, -1):
     for p2_pos in range(10, 0, -1):
         for p1_score in range(20, -1, -1):
@@ -64,8 +64,8 @@ lines = Path('21/input.txt').read_text().strip('\n').split('\n')
 player_1 = int(lines[0].split(' ')[-1])
 player_2 = int(lines[1].split(' ')[-1])
 
-# Now simulate using the actual state
+# Simulate using the actual state
 state = State(player_1, 0, player_2, 0, 1)
 outcome = simulate(state)
 
-print('Outcome:', outcome.player_1_wins if outcome.player_1_wins > outcome.player_2_wins else outcome.player_2_wins)
+print('Outcome:', max(outcome.player_1_wins, outcome.player_2_wins))

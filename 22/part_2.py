4e5d45e6507dc74ddf107f1d989a8eb9bb7c9088
Peter De Roovere <peter.deroovere@robojob.eu>
2021-12-22 13:56:58 +0100

    Extra cleanup

--- 22/part_2.py
+++ 22/part_2.py
@@ -7,7 +7,7 @@ class Range:
     low: int
     high: int
 
-    def diff(self, other):
+    def difference(self, other):
         if self.high < other.low or self.low > other.high:
             return [self]
         result = []
@@ -17,7 +17,7 @@ class Range:
             result.append(Range(other.high + 1, self.high))
         return result
 
-    def intersect(self, other):
+    def intersection(self, other):
         low = max(self.low, other.low)
         high = min(self.high, other.high)
         if low <= high:
@@ -27,9 +27,6 @@ class Range:
 
     def has_overlap(self, other):
         return not (self.high < other.low or self.low > other.high)
-
-    def indices(self):
-        return set(range(self.low, self.high + 1))
 
     def __len__(self):
         return self.high + 1 - self.low
@@ -47,30 +44,23 @@ class Box:
     def has_overlap(self, other):
         return self.x.has_overlap(other.x) and self.y.has_overlap(other.y) and self.z.has_overlap(other.z)
 
-    def diff(self, other):
+    def difference(self, other):
         if not self.has_overlap(other):
             return [self]
         result = []
-        for d_x in self.x.diff(other.x):
+        for d_x in self.x.difference(other.x):
             result.append(Box(self.value, d_x, self.y, self.z))
-        remaining_x = self.x.intersect(other.x)
+        remaining_x = self.x.intersection(other.x)
         if remaining_x is None:
             return result
-        for d_y in self.y.diff(other.y):
+        for d_y in self.y.difference(other.y):
             result.append(Box(self.value, remaining_x, d_y, self.z))
-        remaining_y = self.y.intersect(other.y)
+        remaining_y = self.y.intersection(other.y)
         if remaining_y is None:
             return result
-        for d_z in self.z.diff(other.z):
+        for d_z in self.z.difference(other.z):
             result.append(Box(self.value, remaining_x, remaining_y, d_z))
         return result
-
-def parse(line):
-    global limit_val
-    value, limits = line.split(' ')
-    value = 1 if value == 'on' else 0
-    limits = [int(n) for n in re.findall('[-]*[0-9]+', limits)]
-    return Box(value, Range(limits[0], limits[1]), Range(limits[2], limits[3]), Range(limits[4], limits[5]))
 
 def combine(boxes, new_box):
     new_boxes = []
@@ -78,6 +68,13 @@ def combine(boxes, new_box):
         new_boxes += box.diff(new_box)
     new_boxes.append(new_box)
     return new_boxes
+
+def parse(line):
+    global limit_val
+    value, limits = line.split(' ')
+    value = 1 if value == 'on' else 0
+    limits = [int(n) for n in re.findall('[-]*[0-9]+', limits)]
+    return Box(value, Range(limits[0], limits[1]), Range(limits[2], limits[3]), Range(limits[4], limits[5]))
 
 result = []
 lines = Path('22/input.txt').read_text().strip('\n').split('\n')

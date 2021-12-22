79376bb9630845dea45bf9b930d2919d91774c90
Peter De Roovere <peter.deroovere@robojob.eu>
2021-12-22 15:25:12 +0100

    Cuboid instead of Box naming

--- 22/part_2.py
+++ 22/part_2.py
@@ -29,7 +29,7 @@ class Range:
         return self.high + 1 - self.low
 
 @dataclass
-class Box:
+class Cuboid:
     value: int
     x: Range
     y: Range
@@ -43,36 +43,36 @@ class Box:
             return [self]
         result = []
         for d_x in self.x.differences(other.x):
-            result.append(Box(self.value, d_x, self.y, self.z))
+            result.append(Cuboid(self.value, d_x, self.y, self.z))
         remaining_x = self.x.intersection(other.x)
         for d_y in self.y.differences(other.y):
-            result.append(Box(self.value, remaining_x, d_y, self.z))
+            result.append(Cuboid(self.value, remaining_x, d_y, self.z))
         remaining_y = self.y.intersection(other.y)
         for d_z in self.z.differences(other.z):
-            result.append(Box(self.value, remaining_x, remaining_y, d_z))
+            result.append(Cuboid(self.value, remaining_x, remaining_y, d_z))
         return result
 
     def total_value(self):
         return self.value * len(self.x) * len(self.y) * len(self.z)
 
-def combine(boxes, new_box):
-    remaining_boxes = []
-    for box in boxes:
-        remaining_boxes += box.differences(new_box)
-    return remaining_boxes + [new_box]
+def combine(cuboids, new_cuboid):
+    remaining_cuboids = []
+    for cuboid in cuboids:
+        remaining_cuboids += cuboid.differences(new_cuboid)
+    return remaining_cuboids + [new_cuboid]
 
 def parse(line):
     global limit_val
     value, limits = line.split(' ')
     value = 1 if value == 'on' else 0
     limits = [int(n) for n in re.findall('[-]*[0-9]+', limits)]
-    return Box(value, Range(limits[0], limits[1]), Range(limits[2], limits[3]), Range(limits[4], limits[5]))
+    return Cuboid(value, Range(limits[0], limits[1]), Range(limits[2], limits[3]), Range(limits[4], limits[5]))
 
 lines = Path('22/input.txt').read_text().strip('\n').split('\n')
 
-boxes = []
+cuboids = []
 for i, line in enumerate(lines):
-    box = parse(line)
-    boxes = combine(boxes, box)
+    cuboid = parse(line)
+    cuboids = combine(cuboids, cuboid)
 
-print('Answer: ', sum([box.total_value() for box in boxes]))
+print('Answer: ', sum([cuboid.total_value() for cuboid in cuboids]))
